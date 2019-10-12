bb9a096bd2138fdaa7411e535a78a09da154a64b
Tim Hoffmann <2836374+timhoffm@users.noreply.github.com>
2019-10-12 17:32:21 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pptx-blueprint',
+    version='0.1',
+    description='PowerPoint templating engine',
+    url='https://github.com/timhoffm/pptx-blueprint',
+    author='Tim Hoffmann',
+    author_email='tim.hoffmann@mailbox.org',
+    license='MIT',
+    packages=['pptx_blueprint'],
+    )
