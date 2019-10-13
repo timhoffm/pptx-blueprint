7518b0978d6775f3d6b21032c7a1f57243c85dd7
Fahrudin Halilovic <fahrudin.halilovic@gmail.com>
2019-10-13 09:27:15 +0200

    Implement find_shapes and extend example01.pptx file (#7)
    * implement find_shapes method.
    
    * delete main.py
    
    * Refactoring
    
    * merge
    
    * merge
    
    * refactor code PEP8

--- pptx_blueprint/__init__.py
+++ pptx_blueprint/__init__.py
@@ -1,6 +1,6 @@
 import pathlib
 import pptx
-from typing import Union, Iterable
+from typing import Union, Iterable, Tuple
 from pptx.shapes.base import BaseShape
 
 _Pathlike = Union[str, pathlib.Path]
@@ -20,7 +20,7 @@ class Template:
         self._presentation = pptx.Presentation(filename)
         pass
 
-    def replace_text(self, label: str, text: str, *, scope=None) -> None:
+    def replace_text(self, label: str, new_text: str) -> None:
         """Replaces text placeholders on one or many slides.
 
         Args:
@@ -28,7 +28,10 @@ class Template:
             text (str): new content
             scope: None, slide number, Slide object or iterable of Slide objects
         """
-        pass
+        slide_number, tag_name = self._parse_label(label)
+        shapes = self._find_shapes(slide_number, tag_name)
+        for shape in shapes:
+            shape.text = new_text
 
     def replace_picture(self, label: str, filename: _Pathlike) -> None:
         """Replaces rectangle placeholders on one or many slides.
@@ -48,13 +51,36 @@ class Template:
         """
         pass
 
-    def _find_shapes(self, label: str) -> Iterable[BaseShape]:
-        """ Finds all shapes that match the label
+    def _parse_label(self, label: str) -> Tuple[Union[int, str], str]:
+        slide_number, tag_name = label.split(':')
+        return int(slide_number) if slide_number != '*' else slide_number, tag_name
+
+    def _find_shapes(self,
+                     slide_number: Union[int, str],
+                     tag_name: str) -> Iterable[BaseShape]:
+        """Finds all shapes that match the label
 
         Args:
             label (str): label of the placeholder (without curly braces)
         """
-        pass
+        matched_shapes = []
+
+        def _find_shapes_in_slide(slide):
+            return filter(lambda shape: shape.text == f'{{{tag_name}}}', slide.shapes)
+
+        if slide_number == '*':
+            slides = self._presentation.slides
+        else:
+            # in label we are using 1 based indexing
+            slide_index = slide_number - 1
+            if slide_index < 0 or slide_index >= len(self._presentation.slides):
+                raise IndexError(f"Can't find slide number {slide_number}.")
+            slides = [self._presentation.slides[slide_index]]
+
+        for slide in slides:
+            matched_shapes.extend(_find_shapes_in_slide(slide))
+
+        return matched_shapes
 
     def save(self, filename: _Pathlike) -> None:
         """Saves the updated pptx to the specified filepath.
--- tests/test_template.py
+++ tests/test_template.py
@@ -4,12 +4,37 @@ from pathlib import Path
 import pytest
 
 
+@pytest.fixture
+def template():
+    filename = Path(__file__).absolute().parent / '../data/example01.pptx'
+    tpl = Template(filename)
+    return tpl
+
+
 def test_open_template():
-	filename = Path(__file__).absolute().parent / '../data/example01.pptx'
-	tpl = Template(filename)
+    filename = Path(__file__).absolute().parent / '../data/example01.pptx'
+    tpl = Template(filename)
 
 
 def test_open_template_missing():
-	filename = Path(__file__).absolute().parent / '../data/non_existing.pptx'
-	with pytest.raises(FileNotFoundError):
-		tpl = Template(filename)
+    filename = Path(__file__).absolute().parent / '../data/non_existing.pptx'
+    with pytest.raises(FileNotFoundError):
+        tpl = Template(filename)
+
+
+def test_find_shapes_from_all_slides(template):
+    shapes = template._find_shapes('*', 'title')
+    assert len(shapes) == 3
+    for shape in shapes:
+        assert shape.text == "{title}"
+
+
+def test_find_shapes_from_one_slide(template):
+    shapes = template._find_shapes(1, "logo")
+    assert len(shapes) == 1
+    assert shapes[0].text == '{logo}'
+
+
+def test_find_shapes_index_out_of_range(template):
+    with pytest.raises(IndexError):
+        shapes = template._find_shapes(0, 'logo')
